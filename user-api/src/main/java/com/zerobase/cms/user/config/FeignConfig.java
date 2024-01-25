b966248947b5c657c3ee021372f357f41ce370a6
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-01-25 18:40:10 +0900

    feat: mailgun에 대한 email 연동
    무료 계정으로 테스트 시 이메일 등록 반드시 필요.

---
+++ user-api/src/main/java/com/zerobase/cms/user/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.zerobase.cms.user.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("api", "d78c445964ee22c5c25eda0f5bbfc9be-063062da-ddec9e14");
+    }
+}
