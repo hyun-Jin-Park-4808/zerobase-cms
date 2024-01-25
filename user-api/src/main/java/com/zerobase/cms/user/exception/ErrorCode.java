b7229a185d4150e9fd518579b1078350d0607052
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-01-25 21:29:47 +0900

    feat: 예외처리 핸들러 구현

---
+++ user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.zerobase.cms.user.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+    ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
+    ALREADY_VERIFY(HttpStatus.BAD_REQUEST, "이미 인증이 완료되었습니다."),
+    WRONG_VERIFICATION(HttpStatus.BAD_REQUEST, "잘못된 인증 시도입니다."),
+    EXPIRED_CODE(HttpStatus.BAD_REQUEST, "인증 시간이 만료되었습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+}
