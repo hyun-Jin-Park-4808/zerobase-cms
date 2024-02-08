4669377fd0ca9f7dcaeeb137c68c058d061bc0ef
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-02-08 16:19:47 +0900

    feat: 고객에 대한 잔액 조회 구현

--- user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
+++ user-api/src/main/java/com/zerobase/cms/user/controller/CustomerController.java
@@ -2,17 +2,19 @@ package com.zerobase.cms.user.controller;
 
 import static com.zerobase.cms.user.exception.ErrorCode.*;
 
+import com.zerobase.cms.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.cms.user.domain.customer.CustomerDto;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.exception.CustomException;
-import com.zerobase.cms.user.exception.ErrorCode;
-import com.zerobase.cms.user.service.CustomerService;
+import com.zerobase.cms.user.service.customer.CustomerBalanceService;
+import com.zerobase.cms.user.service.customer.CustomerService;
 import com.zerobase.domain.common.UserVo;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
-import java.util.function.LongFunction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,7 @@ public class CustomerController {
 
     private final JwtAuthenticationProvider provider; // provider를 매개변수로 가지는 CustomerController 생성자 필요
     private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
 
     @GetMapping("/getInfo")
     public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTH-TOKEN") String token) {
@@ -33,4 +36,10 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerDto.from(customer));
     }
 
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTH-TOKEN") String token,
+                                                 @RequestBody ChangeBalanceForm form) {
+        UserVo vo = provider.getUserVo(token);
+        return ResponseEntity.ok(customerBalanceService.changeBalance(vo.getId(), form).getCurrentMoney());
+    }
 }
