7e79db77e5ffdb26c449786b3f123652fde994cd
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-02-08 23:04:16 +0900

    feat: Product Add, ProductItem Add
    추가로 Exception에 대한 Response의 visuality 향상

--- order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
+++ order-api/src/main/java/com/zerobase/cms/order/exception/ErrorCode.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum ErrorCode {
-    ALREADY_REGISTERED_USER(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
+    NOT_FOUND_PRODUCT(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+    SAME_ITEM_NAME(HttpStatus.NOT_FOUND, "아이템 명 중복 입니다.")
     ;
-
 
     private final HttpStatus httpStatus;
     private final String detail;
