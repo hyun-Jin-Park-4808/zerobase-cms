1ca2a38a9cb874facbd364396ae684eaae3cfb8d
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-02-14 16:10:11 +0900

    feat: 결제하기 구현

--- order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
+++ order-api/src/main/java/com/zerobase/cms/order/application/CartApplication.java
@@ -30,7 +30,7 @@ public class CartApplication {
         }
         Cart cart = cartService.getCart(customerId);
 
-        if (cart != null && !addAble(cart, product, form)) {
+        if (!addAble(cart, product, form)) {
             throw new CustomException(ErrorCode.ITEM_COUNT_NOT_ENOUGH);
         }
         return cartService.addCart(customerId, form);
@@ -47,6 +47,7 @@ public class CartApplication {
     // 2. 상품의 가격이나 수량이 변동된다.
     public Cart getCart(Long customerId) {
         Cart cart = refreshCart(cartService.getCart(customerId));
+        cartService.putCart(cart.getCustomerId(), cart);
         Cart returnCart = new Cart();
         returnCart.setCustomerId(customerId);
         returnCart.setProducts(cart.getProducts());
@@ -63,7 +64,7 @@ public class CartApplication {
         cartService.putCart(customerId, null);
     }
 
-    private Cart refreshCart(Cart cart) {
+    protected Cart refreshCart(Cart cart) {
         // 1. 상품이나 상품의 아이템의 정보, 가격, 수량이 변경되었는지 체크하고 알람 제공
         // 2. 상품의 수량, 가격을 우리가 임의로 변경한다.
         Map<Long, Product> productMap = productSearchService.getListByProductIds(
@@ -130,7 +131,6 @@ public class CartApplication {
                 cart.addMessage(builder.toString());
             }
         }
-        cartService.putCart(cart.getCustomerId(), cart);
         return cart;
     }
 
--- order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
+++ order-api/src/main/java/com/zerobase/cms/order/controller/CustomerCartController.java
@@ -1,6 +1,7 @@
 package com.zerobase.cms.order.controller;
 
 import com.zerobase.cms.order.application.CartApplication;
+import com.zerobase.cms.order.application.OrderApplication;
 import com.zerobase.cms.order.domain.product.AddProductCartForm;
 import com.zerobase.cms.order.domain.redis.Cart;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerCartController {
     private final CartApplication cartApplication;
     private final JwtAuthenticationProvider provider;
+    private final OrderApplication orderApplication;
 
     @PostMapping
     public ResponseEntity<Cart> addCart(
@@ -33,9 +36,17 @@ public class CustomerCartController {
         return ResponseEntity.ok(cartApplication.getCart(provider.getUserVo(token).getId()));
     }
 
-    @GetMapping
+    @PutMapping
     public ResponseEntity<Cart> updateCart(
             @RequestHeader(name = "X-AUTH-TOKEN") String token, @RequestBody Cart cart) {
         return ResponseEntity.ok(cartApplication.updateCart(provider.getUserVo(token).getId(), cart));
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<Cart> order(
+            @RequestHeader(name = "X-AUTH-TOKEN") String token,
+            @RequestBody Cart cart) {
+        orderApplication.order(token, cart);
+        return ResponseEntity.ok().build();
     }
 }
